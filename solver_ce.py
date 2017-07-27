f9b10ad17438efdef711a357069692da35eecfb7
Pavan <pavankumar.kota@gmail.com>
2017-07-27 08:29:10 +0530

    routes listed

--- solver_ce.py
+++ solver_ce.py
@@ -51,12 +51,12 @@ def solve_it(input_data):
     for i in customers:
         for j in customers:
             if i.index != j.index:
-                path[i.index,j.index] = m.addVar(vtype=GRB.BINARY, name="Path("+ str(i.index) +", "+ str(j.index) + ")" )
+                path[i.index,j.index] = m.addVar(vtype=GRB.BINARY, name="Path   "+ str(i.index) +"  "+ str(j.index))
 
     vCap = {}
     for i in customers:
         if i.index !=0:
-            vCap[i.index] = m.addVar(lb=i.demand, ub=vehicle_capacity, name="vCap("+str(i.index)+")")
+            vCap[i.index] = m.addVar(lb=i.demand, ub=vehicle_capacity, name="vCap   "+str(i.index))
 
     m.update()
 
@@ -80,9 +80,10 @@ def solve_it(input_data):
 
     m.optimize()
     f = open("./result/solution.csv",'w')
+    print('Variable', file=f)
     for v in m.getVars():
         if v.x != 0:
-            print('%s %g' % (v.varName, v.x), file=f)
+            print('%s' % v.varName, file=f)
 
     # print("Obj: " + str(round(m.ObjVal,3)))
 
@@ -102,8 +103,44 @@ if __name__ == '__main__':
 
         # Reading output file
 
-        data = pd.DataFrame.from_csv("./result/solution.csv")
-        print(data)
+        data = pd.read_csv("./result/solution.csv")
+
+        df = data[data['Variable'].str.startswith('Path')]
+        routes = []
+        for i,row in df.iterrows():
+            x = row['Variable'].split()
+            routes.append([x[1],x[2]])
+        routes_copy = routes[:]
+        route_counter = 0
+        for i in routes:
+            if str(i[0]) == '0':
+                route_counter+=1
+        print("There are " + str(route_counter) + " optimal routes found")
+        route = {}
+        for j in routes_copy:
+            if route_counter > 0 and j[0] == '0':
+                route[route_counter] = j
+                route_counter -=1
+
+        for key, value in route.items():
+            for i in value:
+                if value[-1] != '0':
+                    for j in routes_copy:
+                        if value[-1] == j[0]:
+                            value.append(j[1])
+
+        print(str(route))
+
+
+
+        # route = []
+        # for i in range(route_counter):
+        #     for j in routes_copy:
+        #         if j[0]=='0':
+        #             route[i+1].append(routes_copy[j])
+        #         elif j
+
+
 
     else:
         print(
