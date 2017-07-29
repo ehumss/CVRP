dcdea0ca2ab8e7cc0fdfb5aaf2c466058d09801e
Pavan <pavankumar.kota@gmail.com>
2017-07-29 16:17:47 +0530

    modularized the code

--- solver_ce.py
+++ solver_ce.py
@@ -9,7 +9,6 @@ from collections import namedtuple
 from bokeh.plotting import figure
 from bokeh.io import output_file, save, show
 
-
 Customer = namedtuple("Customer", ['index', 'demand', 'x', 'y'])
 
 def length(customer1, customer2):
@@ -19,7 +18,6 @@ def solve_it(input_data):
 
     # parse the input
     lines = input_data.split('\n')
-
     parts = lines[0].split()
     customer_count = int(parts[0])
     vehicle_count = int(parts[1])
@@ -85,12 +83,58 @@ def solve_it(input_data):
         if v.x != 0:
             print('%s' % v.varName, file=f)
 
-    # print("Obj: " + str(round(m.ObjVal,3)))
+    return round(m.ObjVal,3)
 
-    return
+def post_proc(ip_data):
+
+    # Reading the solution file generated
+    data = pd.read_csv("./result/solution.csv")
+
+    # Creating a Pandas data frame from the output file
+    df = data[data['Variable'].str.startswith('Path')]
+
+    # Creating an array of tuples corresponding between two locations
+    routes = []
+    for i, row in df.iterrows():
+        x = row['Variable'].split()
+        routes.append([x[1], x[2]])
+    routes_copy = routes[:]
+
+    # Determining the number of vehicles needed
+    route_counter = 0
+    for i in routes:
+        if str(i[0]) == '0':
+            route_counter += 1
+
+    vehicles = route_counter
+
+    # Creating a dictionary that holds each of the tour/routes
+    tour = {}
+    for j in routes_copy:
+        if route_counter > 0 and j[0] == '0':
+            tour["Route " + str(route_counter)] = j
+            route_counter -= 1
+
+    for key, value in tour.items():
+        ip_data[key] = ""
+        for i in value:
+            if value[-1] != '0':
+                for j in routes_copy:
+                    if value[-1] == j[0]:
+                        value.append(j[1])
+    df = pd.DataFrame.from_dict(tour, orient='index')
+    df = df.T
+
+    for key, value in tour.items():
+        for ind, val in df[key].iteritems():
+            if val is not None:
+                if (int(ind) != 0 and int(val) != 0):
+                    ip_data[key].iloc[int(val)] = int(ind)
+                else:
+                    ip_data[key].iloc[0] = 0
+    return vehicles, ip_data
 
 import sys
-
 if __name__ == '__main__':
     import sys
 
@@ -99,55 +143,30 @@ if __name__ == '__main__':
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
 
-        solve_it(input_data)
+        tour_length = solve_it(input_data)
+
+        print("The total tour lenght is : " + str(tour_length) + " units")
+
+        # Reading the input file to a Pandas dataframe for further processing
+        ip_data = pd.read_csv(file_location, sep=' ', header=None, skiprows=[0], skip_blank_lines=True,
+                              names=['Demand', 'x-cor', 'y-cor']).dropna(axis=0)
+
+        vehicles, op_data = post_proc(ip_data)
+
+        print("The optimal solution is to use " + str(vehicles) + " vehicles for this problem")
+        print("")
+        print("The path to be followed is shown in the below grid: ")
+        print(op_data)
 
         # Reading output file
 
-
-        ip_data = pd.read_csv(file_location, sep=' ', header=None, skiprows=[0], skip_blank_lines=True,
-                         names=['Demand', 'x-cor', 'y-cor']).dropna(axis=0)
-
-        data = pd.read_csv("./result/solution.csv")
-
-        df = data[data['Variable'].str.startswith('Path')]
-        print(df)
-        routes = []
-        for i,row in df.iterrows():
-            x = row['Variable'].split()
-            routes.append([x[1],x[2]])
-        routes_copy = routes[:]
-        route_counter = 0
-        for i in routes:
-            if str(i[0]) == '0':
-                route_counter+=1
-        print("There are " + str(route_counter) + " optimal routes found")
-        route = {}
-        for j in routes_copy:
-            if route_counter > 0 and j[0] == '0':
-                route["Route " + str(route_counter)] = j
-                route_counter -=1
-
-        for key, value in route.items():
-            ip_data[key] = ""
-            for i in value:
-                if value[-1] != '0':
-                    for j in routes_copy:
-                        if value[-1] == j[0]:
-                            value.append(j[1])
-        df = pd.DataFrame.from_dict(route,orient='index')
-        df = df.T
-        print(df)
-
-        for key, value in route.items():
-            for ind, val in df[key].iteritems():
-                if val is not None:
-                    if (int(ind) != 0 and int(val) != 0):
-                        ip_data[key].iloc[int(val)] = int(ind)
-                    else:
-                        ip_data[key].iloc[0] = 0
     else:
         print(
             'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/vrp_5_4_1)')
+
+
+
+
 
 
 
